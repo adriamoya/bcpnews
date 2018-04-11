e77cef8065613de5e8f62e359f8c1a68eb2a5376
amoya <amoya@amoya>
2018-04-11 15:25:48 +0200

    4-Analysis email articles scraped

--- newspaper_scraper/download_email_articles.py
+++ newspaper_scraper/download_email_articles.py
@@ -8,8 +8,8 @@ from article_scraper import ArticleScraper
 
 # I/O files.
 
-input_file_path = '../expansion_hemeroteca/articles.json'
-output_file 	= codecs.open('articles_standar.json', 'w', encoding='utf-8')
+input_file_path = '../mail_download/emails.json'
+output_file 	= codecs.open('articles_email.json', 'w', encoding='utf-8')
 
 
 # Read data.
@@ -23,9 +23,13 @@ with open(input_file_path) as input_file:
 
 # Download, parse and store articles.
 
-for article in data:
-	new_article = ArticleScraper(article['url'], output_file)
-
+for email in data:
+	try:
+		if len(email['urls']) > 0:
+			for url in email['urls']:
+				new_article = ArticleScraper(url, output_file)
+	except:
+		pass
 
 # Close output file.
 
