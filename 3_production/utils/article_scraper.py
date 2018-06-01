e6c2509f4a3ffd11b5b64fdc7927636e95553d3c
adriamoya <adriamoyaortiz@gmail.com>
2018-06-01 11:50:00 +0200

    46-Article scraper parse()

--- 3_production/utils/article_scraper.py
+++ 3_production/utils/article_scraper.py
@@ -30,6 +30,84 @@ class ArticleScraper(Article):
 			self.parse_article()
 
 
+	def parse(self):
+
+		self.throw_if_not_downloaded_verbose()
+
+		self.doc = self.config.get_parser().fromstring(self.html)
+		self.clean_doc = copy.deepcopy(self.doc)
+
+		if self.doc is None:
+			# `parse` call failed, return nothing
+			return
+
+		# TODO: Fix this, sync in our fix_url() method
+		parse_candidate = self.get_parse_candidate()
+		self.link_hash = parse_candidate.link_hash  # MD5
+
+		document_cleaner = DocumentCleaner(self.config)
+		output_formatter = OutputFormatter(self.config)
+
+		title = self.extractor.get_title(self.clean_doc)
+		self.set_title(title)
+
+		authors = self.extractor.get_authors(self.clean_doc)
+		self.set_authors(authors)
+
+		meta_lang = self.extractor.get_meta_lang(self.clean_doc)
+		self.set_meta_language(meta_lang)
+
+		if self.config.use_meta_language:
+			self.extractor.update_language(self.meta_lang)
+			output_formatter.update_language(self.meta_lang)
+
+		meta_favicon = self.extractor.get_favicon(self.clean_doc)
+		self.set_meta_favicon(meta_favicon)
+
+		meta_description = \
+			self.extractor.get_meta_description(self.clean_doc)
+		self.set_meta_description(meta_description)
+
+		canonical_link = self.extractor.get_canonical_link(
+		self.url, self.clean_doc)
+		self.set_canonical_link(canonical_link)
+
+		tags = self.extractor.extract_tags(self.clean_doc)
+		self.set_tags(tags)
+
+		meta_keywords = self.extractor.get_meta_keywords(
+			self.clean_doc)
+		self.set_meta_keywords(meta_keywords)
+
+		meta_data = self.extractor.get_meta_data(self.clean_doc)
+		self.set_meta_data(meta_data)
+
+		self.publish_date = self.extractor.get_publishing_date(
+			self.url,
+			self.clean_doc)
+
+		# Before any computations on the body, clean DOM object
+		self.doc = document_cleaner.clean(self.doc)
+
+		self.top_node = self.extractor.calculate_best_node(self.doc)
+		if self.top_node is not None:
+			video_extractor = VideoExtractor(self.config, self.top_node)
+			self.set_movies(video_extractor.get_videos())
+
+			self.top_node = self.extractor.post_cleanup(self.top_node)
+			self.clean_top_node = copy.deepcopy(self.top_node)
+
+			text, article_html = output_formatter.get_formatted(
+				self.top_node)
+			self.set_article_html(article_html)
+			self.set_text(text)
+
+		self.fetch_images()
+
+		self.is_parsed = True
+		self.release_resources()
+
+
 	def my_converter(self, o):
 
 		''' Convert datetime to unicode (str) '''
