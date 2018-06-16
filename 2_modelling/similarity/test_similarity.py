305678903799879e70bba015eb1aeadfd5cfbd9b
adriamoya <adriamoyaortiz@gmail.com>
2018-06-16 10:09:16 +0200

    52-Similarities

--- 2_modelling/similarity/test_similarity.py
+++ 2_modelling/similarity/test_similarity.py
@@ -17,3 +17,42 @@ sim = Similarities(df)
 sim.build_similarity_matrix()
 # sim.find_similar_articles()
 sim.return_similar_articles(threshold=0.5, verbose=True)
+
+import nltk
+import string
+import os
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem.porter import PorterStemmer
+
+token_dict = {}
+stemmer = PorterStemmer()
+translator = str.maketrans('', '', string.punctuation)
+
+def stem_tokens(tokens, stemmer):
+	stemmed = []
+	for item in tokens:
+		stemmed.append(stemmer.stem(item))
+	return stemmed
+
+def tokenize(text):
+	tokens = nltk.word_tokenize(text)
+	stems = stem_tokens(tokens, stemmer)
+	return stems
+
+with open('../../3_production/output/20180523_articles.json', encoding='utf-8') as f:
+	for idx, line in enumerate(f):
+		if idx < 3:
+			article_obj = json.loads(line)
+			text = article_obj['text']
+			lowers = text.lower()
+			no_punctuation = lowers.translate(translator)
+			token_dict[idx] = no_punctuation
+
+#this can take some time
+tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
+tfs = tfidf.fit_transform(token_dict.values())
+
+# response = tfidf.transform(token_dict.values())
+print(tfidf.vocabulary_)
+print(tfs)
